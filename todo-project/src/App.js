6981243c3b41be2d5765680cb267d4e857bfa30e
Miqueias dos Santos Sousa Assad <mi.queias2010@hotmail.com>
2023-04-07 14:41:34 -0300

    first component and concepts

--- todo-project/src/App.js
+++ todo-project/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 
+import FirstComponent from './components/FirstComponent';
+
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <FirstComponent/>
     </div>
   );
 }
---
+++ todo-project/src/components/FirstComponent.js
@@ -0,0 +1,19 @@
+function FirstComponent() {
+    // any comment
+    const name = "Miqueias";
+    
+    return (
+        <div className="first-component">
+            {/* any comment in jsx */}
+            <p>First Component</p>
+            <label htmlFor="name">Nome:</label>
+            <input type="text" name="name"></input>
+            {/* template expression */}
+            {2 + 2}
+
+            <p>Name: {name}</p>
+        </div>
+    )
+}
+
+export default FirstComponent;
