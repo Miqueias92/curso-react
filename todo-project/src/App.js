7626d9a5b8f9a9b3db7e2db1e176042526413589
Miqueias dos Santos Sousa Assad <mi.queias2010@hotmail.com>
2023-04-08 21:01:05 -0300

    React children concepts

--- todo-project/src/App.js
+++ todo-project/src/App.js
@@ -7,6 +7,7 @@ import Hooks from './components/Hooks';
 import List from './components/List';
 import RenderCond from './components/RenderCond';
 import Fragment from './components/Fragment';
+import Container from './components/Container';
 
 function App() {
   return (
@@ -19,6 +20,9 @@ function App() {
       <List/>
       <RenderCond x={2} y={10}/>
       <Fragment/>
+      <Container>
+        <h1>this is the child of the container</h1>
+      </Container>
     </div>
   );
 }
