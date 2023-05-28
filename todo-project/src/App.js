9bb85006e033081ffd7fd2c5453c3ae72c13928d
Miqueias dos Santos Sousa Assad <mi.queias2010@hotmail.com>
2023-05-28 14:24:54 -0300

    load todos before on load page

--- todo-project/src/App.js
+++ todo-project/src/App.js
@@ -12,7 +12,25 @@ function App() {
   const [todos, setTodos] = useState([])
   const [loading, setLoading] = useState(false)
 
-  const handleSubmit = (e) => {
+  // load todos before on page load
+  useEffect(() => {
+    const loadData = async() => {
+      setLoading(true)
+
+      const res = await fetch(`${API}/todos`)
+        .then((res)=>res.json())
+        .then((data) => data)
+        .catch((err) => console.error(err));
+      
+      setLoading(false)
+
+      setTodos(res)
+    }
+
+    loadData()
+  }, [])
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     const todo = {
@@ -22,7 +40,15 @@ function App() {
       done: false
     }
 
-    console.log(todo);
+    await fetch(API + '/todos', {
+      method: 'POST',
+      body: JSON.stringify(todo),
+      headers: {
+        'Content-Type': 'application/json',
+      } 
+    })
+
+    setTodos((prevState) => [...prevState, todo])
 
     setTitle("")
     setTime("")
@@ -64,6 +90,11 @@ function App() {
       <div className='list-todo'>
         <h2>Lista de tarefas:</h2>
         {todos.length === 0 && <p>Não há tarefas</p>}
+        {todos.map((todo) => (
+          <div className='todo' key={todo.id}>
+            <p>{todo.title}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
